8448ed659373a9b1cc94e80079e8fd5d76c8d4c8
Rian <rian.ace.2003@gmail.com>
2023-05-06 13:56:41 -0300

    finalização do carrossel da landing page

--- src/app/components/utilities/carrossel/carrossel.component.ts
+++ src/app/components/utilities/carrossel/carrossel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carrossel',
@@ -8,27 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class CarrosselComponent {
 
   valor: String = '-100%';
-  telas: Array<any> = [
-    {color: 'red'},
-    {color: 'blue'},
-    {color: 'yellow'},
-    {color: 'brown'}
-  ]
+  @Input() telas: Array<{ title: string, url: String, text: String }> = [];
 
-  teste2() {
-    console.log('test')
+  async ngOnInit() {
+
+    let containerCarrossel = await document.querySelector('.container-carrossel');
+    let primeiroBotao: HTMLElement = containerCarrossel?.querySelector('button') as HTMLElement;
+
+    primeiroBotao.classList.add('btn-active');
+
   }
 
-  teste(te: any, event: Event) {
+  slide(te: any, event: Event) {
     let carrossel: HTMLElement = document.querySelector('.carrossel') as HTMLElement;
     let el: HTMLElement = event.target as HTMLElement;
     let listaBtn: NodeListOf<HTMLButtonElement> = document.querySelectorAll(`.${el.classList[0]}`);
+
     for(let btn of Array.from(listaBtn)) {
       if(btn.classList.contains('btn-active')) {
         btn.classList.remove('btn-active');
       }
     }
-    el.classList.add('btn-active')
+
+    el.classList.add('btn-active');
+
     for(let i in this.telas) {
       if(this.telas[i] == te){
         carrossel.style.left = `-${Number(i)*100}%`
