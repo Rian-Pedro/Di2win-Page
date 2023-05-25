9efcc7dce7c19794ffe1619f6051376557d846fc
Rian <rian.ace.2003@gmail.com>
2023-05-25 11:52:48 -0300

    adjusting components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,14 +9,17 @@ export class HomeComponent {
 
   advantageList: Array<{num: String, title: String, text: String}> = [
     {num: '01',
-     title: 'Lorem Ipsum',
-     text: 'Incididunt veniam sint et ea aute aliquip enim incididunt est qui cillum. Aute commodo elit voluptate ad nulla enim enim eiusmod incididunt aliqua consectetur cupidatat ut. In aliqua minim ad pariatur sunt nulla eiusmod dolor anim veniam enim cupidatat est veniam. Quis occaecat aliqua do ullamco fugiat occaecat magna nisi.'},
-     {num: '01',
-     title: 'Lorem Ipsum',
-     text: 'Incididunt veniam sint et ea aute aliquip enim incididunt est qui cillum. Aute commodo elit voluptate ad nulla enim enim eiusmod incididunt aliqua consectetur cupidatat ut. In aliqua minim ad pariatur sunt nulla eiusmod dolor anim veniam enim cupidatat est veniam. Quis occaecat aliqua do ullamco fugiat occaecat magna nisi.'},
-     {num: '01',
-     title: 'Lorem Ipsum',
-     text: 'Incididunt veniam sint et ea aute aliquip enim incididunt est qui cillum. Aute commodo elit voluptate ad nulla enim enim eiusmod incididunt aliqua consectetur cupidatat ut. In aliqua minim ad pariatur sunt nulla eiusmod dolor anim veniam enim cupidatat est veniam. Quis occaecat aliqua do ullamco fugiat occaecat magna nisi.'}
+     title: 'RECEBÍVEIS',
+     text: 'Por meio da ferramenta é possível acelerar o processo de extração das informações de duplicatas, contratos de aluguel, hipotecas, notas fiscais de serviços, precatórios, debêntures e outros recebíveis.'},
+     {num: '02',
+     title: 'Documentos Societários',
+     text: 'Por meio da ferramenta é possível extrair dos documentos societários informações de pessoas, poderes, bens, cotas, etc.'},
+     {num: '03',
+     title: 'Documento Contábil',
+     text: 'Através dos demonstrativos da empresa como balanço patrimonial, DRE, balancetes e outros demonstrativos é possível extrair as informações contábeis e aplicar as análises financeiras de endividamento, liquidez, rentabilidade, curva ABC, faturamento, entre outras análises.'},
+     {num: '04',
+      title: ' Documentos Pessoais',
+      text: 'Nos documentos estruturados como CNH, RG, IRPF, Comprovante de Residência, Certidão de Casamento, Certidão de Nascimento, podemos extrair dados pessoais de endereço, nomes, datas, valores, entre outras informações que constem nestes documentos e sejam necessárias'}
   ]
 
 }
