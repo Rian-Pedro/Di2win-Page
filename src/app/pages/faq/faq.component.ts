efd946b78f792d51f89ecd20eecc98329fc18949
Rian <rian.ace.2003@gmail.com>
2023-06-10 21:01:20 -0300

    update content

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -9,13 +9,13 @@ export class FaqComponent {
 
   questions: Array<{title: string, content: string}> = [
     { title: "O que é o extrAIdados?",
-      content: "O produto utiliza Inteligência Artificial de Machine Learnig e OCR para extrair e processar os dados, o que permite treinar a ferramenta para aprender com os dados, identificar padrões e tomar decisões com o mínimo de intervenção humana." },
+      content: "O produto utiliza Inteligência Artificial de Machine Learnig e OCR para extrair e processar os dados, o que permite treinar o produto para aprender com os dados, identificar padrões e tomar decisões com o mínimo de intervenção humana." },
     { title: "Quais tipos de documentos podem ser utilizados para extração de dados?",
       content: "Podem ser documentos estruturados, semiestruturados e não estruturados, nos formatos em PDF e imagem (JPEG, PNG)." },
     { title: "Os dados extraídos podem ser estruturados e devolvidos em que formatos?",
       content: "JSON, DOCX, TXT, PDF, DOC e XLS." },
-    { title: "Posso acessar a ferramenta em qualquer dispositivo?",
-      content: "A ferramenta pode ser acessada por mobile, desktop, tablet ou notebook." },
+    { title: "Posso acessar o produto em qualquer dispositivo?",
+      content: "O produto pode ser acessado por mobile, desktop, tablet ou notebook." },
     { title: "Como ter acesso ao produto e qual o custo de utilização?",
       content: "Através do contato com nossa equipe você pode solicitar uma demonstração gratuita e testar o produto. O custo é por volume de extração realizada, sendo adequado a operação de cada usuário." },
     { title: "Como saber a quantidade de páginas que já utilizei do meu contrato?",
@@ -25,7 +25,7 @@ export class FaqComponent {
     { title: "Quem tem acesso aos meus dados processados?",
       content: "Os dados são confidenciais, com restrição e controle de acesso na disponibilidade das informações às pessoas autorizadas. O usuário é validado pelo sistema e autorizado de acordo com as permissões a ele atribuídas." },
     { title: "Quais procedimentos de segurança são tomados com relação ao conteúdo extraído?",
-      content: "A ferramenta ExtrAIdados está em conformidade com a LGPD, garantindo a proteção dos dados pessoais. Com ela, é possível extrair informações de forma segura e transparente, respeitando os direitos de privacidade dos usuários." }
+      content: "O produto ExtrAIdados está em conformidade com a LGPD, garantindo a proteção dos dados pessoais. Com ela, é possível extrair informações de forma segura e transparente, respeitando os direitos de privacidade dos usuários." }
   ]
 
   openResponse(event: any) {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,32 +10,32 @@ export class HomeComponent {
   arrayCarrossel: Array<{ title: string, url: string, text: string }> = [
       { title: 'financeiro',
         url: '../../../../assets/soluções/financeiro.svg',
-        text: 'A tecnologia de ponta da extrAIdados agiliza a coleta de dados, reduzindo erros e melhorando a eficiência operacional. Com essa ferramenta, empresas podem tomar decisões mais embasadas, maximizar receitas e minimizar riscos.'
+        text: 'A tecnologia de ponta da extrAIdados agiliza a coleta de dados, reduzindo erros e melhorando a eficiência operacional. Com esse produto, empresas podem tomar decisões mais embasadas, maximizar receitas e minimizar riscos.'
       },
 
       { title: 'imobiliário',
         url: '../../../../assets/soluções/imobiliario.svg',
-        text: 'A ferramenta simplifica automatizando a extração de registros de propriedades, contratos e transações. Isso permite aos profissionais otimizar processos, eliminando tarefas administrativas repetitivas.'
+        text: 'O produto simplifica automatizando a extração de registros de propriedades, contratos e transações. Isso permite aos profissionais otimizar processos, eliminando tarefas administrativas repetitivas.'
       },
 
       { title: 'educação',
         url: '../../../../assets/soluções/educacao.svg',
-        text: 'A ferramenta extrAIdados simplifica a gestão administrativa das instituições educacionais, automatizando processos como matrículas, registros de alunos e fluxo de documentos. Isso permite que as equipes se concentrem no ensino e na melhoria da experiência dos alunos.'
+        text: 'O produto extrAIdados simplifica a gestão administrativa das instituições educacionais, automatizando processos como matrículas, registros de alunos e fluxo de documentos. Isso permite que as equipes se concentrem no ensino e na melhoria da experiência dos alunos.'
       },
 
       { title: 'seguros',
         url: '../../../../assets/soluções/seguros.svg',
-        text: 'O extrAIdados oferece automação na coleta de dados. Com a ferramenta as companhias de seguros podem agilizar o processamento de sinistros, avaliar riscos com maior precisão e fornecer um serviço mais eficiente aos segurados.'
+        text: 'O extrAIdados oferece automação na coleta de dados. Com o produto as companhias de seguros podem agilizar o processamento de sinistros, avaliar riscos com maior precisão e fornecer um serviço mais eficiente aos segurados.'
       }
   ]
 
   advantageList: Array<{num: String, title: String, text: String}> = [
     {num: '01',
      title: 'RECEBÍVEIS',
-     text: 'Por meio da ferramenta é possível acelerar o processo de extração das informações de duplicatas, contratos de aluguel, hipotecas, notas fiscais de serviços, precatórios, debêntures e outros recebíveis.'},
+     text: 'Por meio do produto é possível acelerar o processo de extração das informações de duplicatas, contratos de aluguel, hipotecas, notas fiscais de serviços, precatórios, debêntures e outros recebíveis.'},
      {num: '02',
      title: 'Documentos Societários',
-     text: 'Por meio da ferramenta é possível extrair dos documentos societários informações de pessoas, poderes, bens, cotas, etc.'},
+     text: 'Por meio do produto é possível extrair dos documentos societários informações de pessoas, poderes, bens, cotas, etc.'},
      {num: '03',
      title: 'Documento Contábil',
      text: 'Através dos demonstrativos da empresa como balanço patrimonial, DRE, balancetes e outros demonstrativos é possível extrair as informações contábeis e aplicar as análises financeiras de endividamento, liquidez, rentabilidade, curva ABC, faturamento, entre outras análises.'},
