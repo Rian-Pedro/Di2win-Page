12038808296c1175744fb34d83a7350ef1872ce0
Rian <rian.ace.2003@gmail.com>
2023-06-10 22:11:15 -0300

    update content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -30,16 +30,13 @@ export class HomeComponent {
   ]
 
   advantageList: Array<{num: String, title: String, text: String}> = [
-    {num: '01',
-     title: 'RECEBÍVEIS',
-     text: 'Por meio do produto é possível acelerar o processo de extração das informações de duplicatas, contratos de aluguel, hipotecas, notas fiscais de serviços, precatórios, debêntures e outros recebíveis.'},
-     {num: '02',
+     {num: '01',
      title: 'Documentos Societários',
      text: 'Por meio do produto é possível extrair dos documentos societários informações de pessoas, poderes, bens, cotas, etc.'},
-     {num: '03',
+     {num: '02',
      title: 'Documento Contábil',
      text: 'Através dos demonstrativos da empresa como balanço patrimonial, DRE, balancetes e outros demonstrativos é possível extrair as informações contábeis e aplicar as análises financeiras de endividamento, liquidez, rentabilidade, curva ABC, faturamento, entre outras análises.'},
-     {num: '04',
+     {num: '03',
       title: ' Documentos Pessoais',
       text: 'Nos documentos estruturados como CNH, RG, IRPF, Comprovante de Residência, Certidão de Casamento, Certidão de Nascimento, podemos extrair dados pessoais de endereço, nomes, datas, valores, entre outras informações que constem nestes documentos e sejam necessárias'}
   ]
